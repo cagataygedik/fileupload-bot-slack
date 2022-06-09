a8a3454d488e4d10328db87ecd0d85c848a32b5b
cagataygedik <cagatay.gedik@icloud.com>
2022-06-09 11:54:58 +0300

    finished

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3635365776915-3637977500947-UGDGLRgVLaRqJwChtdSA4AUD")
+	os.Setenv("CHANNEL_ID", "C03J8QBD2CF")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"Document-untitled.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s URL: %s\n", file.Name, file.URL)
+	}
+}
